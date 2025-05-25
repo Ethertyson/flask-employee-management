1aa51ff1c8285494b64a3a390a6169d47b2ea051
Pritanshu Srivastava <pritanshusrivastava24880@gmail.com>
2025-05-26 04:23:00 +0530

    Fix bug causing 502 error in deployment

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 # Created by Pritanshu on 2025-05-20
 
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
